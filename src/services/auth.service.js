ea525a6f99b7e77fe4caba5698981e39f96e7327
Ahmad Abdullahi <ahmadwiesman@gmail.com>
2024-09-19 11:20:06 +0100

    nav

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -22,19 +22,19 @@ class AuthService {
   login = (requestBody) => {
     return this.api.post("/auth/login", requestBody);
     // same as
-    // return axios.post("http://localhost:5005/auth/login");
+    // return axios.post("http://localhost:5000/auth/login");
   };
 
   signup = (requestBody) => {
     return this.api.post("/auth/signup", requestBody);
     // same as
-    // return axios.post("http://localhost:5005/auth/singup");
+    // return axios.post("http://localhost:5000/auth/singup");
   };
 
   verify = () => {
     return this.api.get("/auth/verify");
     // same as
-    // return axios.post("http://localhost:5005/auth/verify");
+    // return axios.post("http://localhost:5000/auth/verify");
   };
 }
 
